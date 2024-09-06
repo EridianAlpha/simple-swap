cb907eb44e6432866fd019044007ef1b69135484
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-09-06 16:34:45 +0100

    Add SimpleSwapInternalFunctionsHelper

---
+++ src/internalFunctionHelpers/SimpleSwapInternalFunctionsHelper.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {SimpleSwap} from "../SimpleSwap.sol";
+
+/// @notice This contract is used to test the internal functions of the SimpleSwap contract.
+contract SimpleSwapInternalFunctionsHelper is SimpleSwap {}
